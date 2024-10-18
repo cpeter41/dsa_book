00b477f11f0d9613d8434172fc66096f9ac926c6
cpeter41 <chris.steven.peters@gmail.com>
2024-10-17 22:37:42 -0700

    215, first heap

---
+++ Heap/215_Kth_Largest_Element_in_an_Array.py
@@ -0,0 +1,20 @@
+# doing this in python because it has a built in heap structure
+# can implement either a heap class or treat an array as a heap
+# both take a long time
+
+import heapq
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        value = None
+        for i in range(len(nums) - k + 1):
+            value = heapq.heappop(nums)
+            print(value)
+
+        return value
+    
+# technically python has a built in function that does this:
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return heapq.nlargest(k, nums)[k - 1]
